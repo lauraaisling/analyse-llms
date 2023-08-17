9004cb3bd9a453e140b3dc076338035cc77a4f7b
lomahony <lauraomahony999@gmail.com>
2023-08-17 16:45:01 +0000

    update calcs

--- README.md
+++ README.md
@@ -1,17 +1,35 @@
 # analyse-llms
 
+Compute token average probabilities and intermediate outputs for calculating entropy, perplexity (e.g. logprobs)
 ```bash
-# Compute intermediate outputs for calculating perplexity (e.g. logprobs)
-python scripts/calculation_data.py \
+python scripts/compute_data.py \
     --model_config_path preset_configs/pythia70m-sft.json \
     --data_path data/val.jsonl.zst \
-    --calculation_output_path outputs/pythia70m-sft_calculation_data.p
+    --calculation_output_path outputs/pythia70m-sft_calculation_data_probs50000.p \
+    --calc_probs True
 ```
 
-# Use intermediate outputs to compute perplexity
+Use intermediate outputs to compute perplexity
 ```bash
-python scripts/compute_perplexity.py \
-    --calculation_output_path outputs/pythia70m-sft-calculation_data.p \
+python scripts/calc_entropy_perplexity.py \
+    --computation_data_path outputs/pythia70m-sft-sft_calculation_data_probs50000.p \
     --perplexity_output_path outputs/pythia70m-sft-perplexity.json
 ```
 
+
+Set up: 
+
+* Download The Pile validation data as follows: 
+```bash
+wget https://the-eye.eu/public/AI/pile/val.jsonl.zst
+```
+## My notes: 
+Ran: 
+* python scripts/compute_data.py --model_config_path preset_configs/pythia70m-base.json --data_path data/val.jsonl.zst --calculation_output_path outputs/pythia70m-base_calculation_data_probs50000.p
+and sft, dpo
+
+* python scripts/compute_perplexity.py --perplexity_data_path outputs/pythia70m-base_calculation_data_probs50000.p --output_path outputs/pythia70m-base-50000.json
+and sft, dpo
+
+
+utils.py and compute_entropy_perplexity.py adapted from from https://github.com/EleutherAI/lm_perplexity/tree/main. 
---
+++ scripts/compute_data.py
@@ -0,0 +1,84 @@
+import argparse
+import torch
+from tqdm import auto as tqdm_lib
+import numpy as np
+
+import lm_dataformat
+import pythia_funcs 
+import utils
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_config_path', required=True)
+    parser.add_argument('--data_path', required=True)
+    parser.add_argument('--calculation_output_path', required=True)
+    parser.add_argument('--calc_probs', default=False, type=lambda x: (str(x).lower() in ['true', '1', 'yes'])) ### avg_probs_doc 
+    parser.add_argument('--max_docs', type=int, default=None)
+    parser.add_argument('--doc_indices_path', type=str, default=None)
+    return parser.parse_args()
+
+
+def compute_data(model, data_path, calc_probs, indices=None):
+    # For expedience, we're going to assume everything fits in memory for now
+    # Also for expedience we're just going to save lists of arrays
+    overall_output = {
+        "all_logprobs": [],
+        "all_positions": [],
+        # "block_mean_probs": [],
+        "mean_probs": [],
+        "aggregate_length": 0,
+        "aggregate_utf8_length": 0.
+    }
+    temp_output = {
+        "block_mean_probs": [],
+    }
+
+    reader = lm_dataformat.Reader(data_path)
+    for i, doc in enumerate(tqdm_lib.tqdm(reader.stream_data())):
+        # testing on just this chunk (~1/3 of Pile)! 
+        if i < 50000: 
+            if indices is not None and i not in indices:
+                continue
+            output = model.get_compute_data(doc, calc_probs) ### 
+            if not output:
+                continue
+            overall_output["all_logprobs"].append(output["logprobs"])
+            overall_output["all_positions"].append(output["positions"])
+            overall_output["aggregate_length"] += output["length"]
+            overall_output["aggregate_utf8_length"] += output["utf8_length"]
+            temp_output["block_mean_probs"].append(output["block_mean_probs"])
+
+            if i % 500==0: 
+                # mean_probs = np.concatenate(overall_output["block_mean_probs"])
+                overall_output["mean_probs"].append(np.concatenate(temp_output["block_mean_probs"])) 
+                temp_output["block_mean_probs"] = []
+        else:
+            break
+
+    return overall_output
+
+
+def main():
+    args = parse_args()
+    print(f'args.calc_probs" {args.calc_probs}')
+    model = pythia_funcs.create_model(args.model_config_path)
+    if args.doc_indices_path:
+        assert args.max_docs is None
+        indices = set(utils.read_json(args.doc_indices_path))
+    elif args.max_docs:
+        assert args.doc_indices_path is None
+        indices = set(range(args.max_docs))
+    else:
+        indices = None
+    perplexity_data = compute_data(
+        model=model,
+        data_path=args.data_path,
+        calc_probs=args.calc_probs, # 
+        indices=indices,
+    )
+    torch.save(perplexity_data, args.calculation_output_path)
+
+
+if __name__ == "__main__":
+    main()

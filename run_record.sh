3ea53b2d70574fcc735fca653ed5430b818c4042
lomahony <lauraomahony999@gmail.com>
2023-09-16 17:16:11 +0000

    update hs

--- run_record.sh
+++ run_record.sh
@@ -49,12 +49,8 @@ source ~/anaconda3/bin/activate analyse-llms1
 # python scripts/jigsaw_toxic_hs.py --hf_model lomahony/eleuther-pythia2.8b-hh-dpo --n 1000 --layers=[-1,-2,-3,-4,-5]
 
 
-python scripts/jigsaw_toxic_hs.py --hf_model EleutherAI/pythia-2.8b --n 1000 --layers=[-1] # ,-2,-3,-4,-5
-python scripts/jigsaw_toxic_hs.py --hf_model lomahony/eleuther-pythia2.8b-hh-sft --n 1000 --layers=[-1] # ,-2,-3,-4,-5
-# python scripts/jigsaw_toxic_hs.py --hf_model lomahony/eleuther-pythia2.8b-hh-dpo --n 1000 --layers=[-1] # ,-2,-3,-4,-5
-
-python scripts/jigsaw_toxic_hs.py --hf_model EleutherAI/pythia-2.8b --n 1000 --layers=[-2,-3,-4,-5] 
-python scripts/jigsaw_toxic_hs.py --hf_model lomahony/eleuther-pythia2.8b-hh-sft --n 1000 --layers=[-2,-3,-4,-5] 
-python scripts/jigsaw_toxic_hs.py --hf_model lomahony/eleuther-pythia2.8b-hh-dpo --n 1000 --layers=[-2,-3,-4,-5] 
+python scripts/jigsaw_toxic_hs.py --hf_model EleutherAI/pythia-2.8b --n 1000 --layers=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]
+python scripts/jigsaw_toxic_hs.py --hf_model lomahony/eleuther-pythia2.8b-hh-sft --n 1000 --layers=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]
+python scripts/jigsaw_toxic_hs.py --hf_model lomahony/eleuther-pythia2.8b-hh-dpo --n 1000 --layers=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]
 
 # bash run_record.sh 

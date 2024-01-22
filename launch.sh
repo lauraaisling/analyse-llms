63c8c730a570bf0f4e438deb1022d5db6f623b8b
lomahony <lauraomahony999@gmail.com>
2024-01-22 07:34:03 +0000

    add results

--- launch.sh
+++ launch.sh
@@ -28,22 +28,22 @@ source ~/anaconda3/bin/activate analyse-llms
 # 160m no probs or confidence
 # python scripts/compute_data.py --model_config_path preset_configs/pythia160m-base.json --data_path data/val.jsonl --calculation_output_path outputs/pythia160m-base-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
 # python scripts/compute_data.py --model_config_path preset_configs/pythia160m-sft.json --data_path data/val.jsonl --calculation_output_path outputs/pythia160m-sft-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
-python scripts/compute_data.py --model_config_path preset_configs/pythia160m-dpo.json --data_path data/val.jsonl --calculation_output_path outputs/pythia160m-dpo-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
+# python scripts/compute_data.py --model_config_path preset_configs/pythia160m-dpo.json --data_path data/val.jsonl --calculation_output_path outputs/pythia160m-dpo-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
 
 # perplexity 160m
 # python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia160m-base-calculation_data10000pc.p --output_path results/pythia160m-base-perplexity.json
 # python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia160m-sft-calculation_data10000pc.p --output_path results/pythia160m-sft-perplexity.json
-python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia160m-dpo-calculation_data10000pc.p --output_path results/pythia160m-dpo-perplexity.json
+# python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia160m-dpo-calculation_data10000pc.p --output_path results/pythia160m-dpo-perplexity.json
 
 # 1b no probs or confidence
-# python scripts/compute_data.py --model_config_path preset_configs/pythia1b-base.json --data_path data/val.jsonl --calculation_output_path outputs/pythia1b-base-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
-# python scripts/compute_data.py --model_config_path preset_configs/pythia1b-sft.json --data_path data/val.jsonl --calculation_output_path outputs/pythia1b-sft-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
-# python scripts/compute_data.py --model_config_path preset_configs/pythia1b-dpo.json --data_path data/val.jsonl --calculation_output_path outputs/pythia1b-dpo-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
+python scripts/compute_data.py --model_config_path preset_configs/pythia1b-base.json --data_path data/val.jsonl --calculation_output_path outputs/pythia1b-base-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
+python scripts/compute_data.py --model_config_path preset_configs/pythia1b-sft.json --data_path data/val.jsonl --calculation_output_path outputs/pythia1b-sft-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
+python scripts/compute_data.py --model_config_path preset_configs/pythia1b-dpo.json --data_path data/val.jsonl --calculation_output_path outputs/pythia1b-dpo-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
 
-# # perplexity 1b
-# python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia1b-base-calculation_data10000pc.p --output_path results/pythia1b-base-perplexity.json
-# python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia1b-sft-calculation_data10000pc.p --output_path results/pythia1b-sft-perplexity.json
-# python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia1b-dpo-calculation_data10000pc.p --output_path results/pythia1b-dpo-perplexity.json
+# perplexity 1b
+python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia1b-base-calculation_data10000pc.p --output_path results/pythia1b-base-perplexity.json
+python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia1b-sft-calculation_data10000pc.p --output_path results/pythia1b-sft-perplexity.json
+python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia1b-dpo-calculation_data10000pc.p --output_path results/pythia1b-dpo-perplexity.json
 
 # 1.4b no probs or confidence
 # python scripts/compute_data.py --model_config_path preset_configs/pythia14b-base.json --data_path data/val.jsonl --calculation_output_path outputs/pythia14b-base-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
@@ -56,14 +56,14 @@ python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pyth
 # python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia14b-dpo-calculation_data10000pc.p --output_path results/pythia14b-dpo-perplexity.json
 
 # 2.8b no probs or confidence
-python scripts/compute_data.py --model_config_path preset_configs/pythia28b-base.json --data_path data/val.jsonl --calculation_output_path outputs/pythia28b-base-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
-python scripts/compute_data.py --model_config_path preset_configs/pythia28b-sft.json --data_path data/val.jsonl --calculation_output_path outputs/pythia28b-sft-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
-python scripts/compute_data.py --model_config_path preset_configs/pythia28b-dpo.json --data_path data/val.jsonl --calculation_output_path outputs/pythia28b-dpo-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
+# python scripts/compute_data.py --model_config_path preset_configs/pythia28b-base.json --data_path data/val.jsonl --calculation_output_path outputs/pythia28b-base-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
+# python scripts/compute_data.py --model_config_path preset_configs/pythia28b-sft.json --data_path data/val.jsonl --calculation_output_path outputs/pythia28b-sft-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
+# python scripts/compute_data.py --model_config_path preset_configs/pythia28b-dpo.json --data_path data/val.jsonl --calculation_output_path outputs/pythia28b-dpo-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000
 
 # perplexity 2.8b
-python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia28b-base-calculation_data10000pc.p --output_path results/pythia28b-base-perplexity.json
-python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia28b-sft-calculation_data10000pc.p --output_path results/pythia28b-sft-perplexity.json
-python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia28b-dpo-calculation_data10000pc.p --output_path results/pythia28b-dpo-perplexity.json
+# python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia28b-base-calculation_data10000pc.p --output_path results/pythia28b-base-perplexity.json
+# python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia28b-sft-calculation_data10000pc.p --output_path results/pythia28b-sft-perplexity.json
+# python scripts/calc_entropy_perplexity.py --calculation_output_path outputs/pythia28b-dpo-calculation_data10000pc.p --output_path results/pythia28b-dpo-perplexity.json
 
 # 6.9b no probs or confidence
 # python scripts/compute_data.py --model_config_path preset_configs/pythia69b-base.json --data_path data/val.jsonl --calculation_output_path outputs/pythia69b-base-calculation_data10000pc.p --calc_confidence True --calc_probs True --max_docs 10000

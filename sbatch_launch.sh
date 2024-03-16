9b11a68aa32ae545312309dd31974820ffee9aac
lomahony <lauraomahony999@gmail.com>
2024-03-16 15:04:13 +0000

    update script co

--- sbatch_launch.sh
+++ sbatch_launch.sh
@@ -2,8 +2,8 @@
 # sbatch sbatch_launch.sh
 #Resource Request 
 #SBATCH --account=eleuther
-#SBATCH --job-name=modes
-#SBATCH --output=slurm-TONAME-%x_%j.out   ## filename of the output; the %j is equivalent to jobID; default is slurm-[jobID].out 
+#SBATCH --job-name=completions
+#SBATCH --output=slurm_%x_pythia-2.8b-dpo_%j.out   ## filename of the output; the %j is equivalent to jobID; default is slurm-[jobID].out 
 #SBATCH --partition=a40x ## the partitions to run in (comma seperated) 
 #SBATCH --gpus=1 # number of gpus per task 
 #SBATCH --cpus-per-gpu=12 
@@ -27,11 +27,30 @@ source ~/venvs/venv-analyse-llms/bin/activate
 # ContextualAI/archangel_sft-dpo_llama7b
 
 ### Running
-# echo 'completions llama-sft'
-# python scripts/creative_and_factual_completions.py
+echo 'completions llama-slic'
+python scripts/creative_and_factual_completions.py --model "llama2-slic"
 
+# echo 'diversity llama-chat 20 words'
+# python scripts/creative_and_factual_metrics.py --model "llama2-chat" --factual_completion_path "results/completions/llama2-chat_completions_factual_max_length25.npy" --creative_completion_path "results/completions/llama2-chat_completions_creative_max_length70.npy"
+# echo 'diversity llama 20 words'
+# python scripts/creative_and_factual_metrics.py --model "llama2" --factual_completion_path "results/completions/llama2_completions_factual_max_length25.npy" --creative_completion_path "results/completions/llama2_completions_creative_max_length70.npy"
 # echo 'diversity llama-sft 20 words'
-# python scripts/creative_and_factual_metrics.py --model "llama2-sft" --factual_completion_path "results/completions/llama2-sft_completions_factual_max_length70.npy" --creative_completion_path "results/completions/llama2-sft_completions_creative_max_length70.npy"
+# python scripts/creative_and_factual_metrics.py --model "llama2-sft" --factual_completion_path "results/completions/llama2-sft_completions_factual_max_length25.npy" --creative_completion_path "results/completions/llama2-sft_completions_creative_max_length70.npy"
+# echo 'diversity llama-dpo 20 words'
+# python scripts/creative_and_factual_metrics.py --model "llama2-dpo" --factual_completion_path "results/completions/llama2-dpo_completions_factual_max_length25.npy" --creative_completion_path "results/completions/llama2-dpo_completions_creative_max_length70.npy"
+# echo 'diversity llama-ppo 20 words'
+# python scripts/creative_and_factual_metrics.py --model "llama2-ppo" --factual_completion_path "results/completions/llama2-ppo_completions_factual_max_length25.npy" --creative_completion_path "results/completions/llama2-ppo_completions_creative_max_length70.npy"
+# echo 'diversity pythia-2.8b 20 words'
+# python scripts/creative_and_factual_metrics.py --model "pythia-2.8b" --factual_completion_path "results/completions/pythia-2.8b_completions_factual_max_length25.npy" --creative_completion_path "results/completions/pythia-2.8b_completions_creative_max_length70.npy"
+# echo 'diversity pythia-2.8b-sft 20 words'
+# python scripts/creative_and_factual_metrics.py --model "pythia-2.8b-sft" --factual_completion_path "results/completions/pythia-2.8b-sft_completions_factual_max_length25.npy" --creative_completion_path "results/completions/pythia-2.8b-sft_completions_creative_max_length70.npy"
+# echo 'diversity pythia-2.8b-dpo 20 words'
+# python scripts/creative_and_factual_metrics.py --model "pythia-2.8b-dpo" --factual_completion_path "results/completions/pythia-2.8b-dpo_completions_factual_max_length25.npy" --creative_completion_path "results/completions/pythia-2.8b-dpo_completions_creative_max_length70.npy"
+
+# echo 'diversity llama-kto 20 words'
+# python scripts/creative_and_factual_metrics.py --model "llama2-kto" --factual_completion_path "results/completions/llama2-kto_completions_factual_max_length25.npy" --creative_completion_path "results/completions/llama2-kto_completions_creative_max_length70.npy"
+echo 'diversity llama-slic 20 words'
+python scripts/creative_and_factual_metrics.py --model "llama2-slic" --factual_completion_path "results/completions/llama2-slic_completions_factual_max_length25.npy" --creative_completion_path "results/completions/llama2-slic_completions_creative_max_length70.npy"
 
 # echo 'diversity llama-sft 5 words'
-# python scripts/creative_and_factual_metrics.py --model "llama2-sft" --factual_completion_path "results/completions/llama2-sft_completions_factual_max_length70.npy" --creative_completion_path "results/completions/llama2-sft_completions_creative_max_length70.npy" --max_num_words 5
+# python scripts/creative_and_factual_metrics.py --model "llama2-kto" --factual_completion_path "results/completions/llama2-sft_completions_factual_max_length70.npy" --creative_completion_path "results/completions/llama2-sft_completions_creative_max_length70.npy" --max_num_words 5
--- scripts/creative_and_factual_completions.py
+++ scripts/creative_and_factual_completions.py
@@ -5,13 +5,16 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, LlamaForCausalLM, 
 import torch
 import argparse
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--model', type=str, help='Model must be in [llama2, llama2-chat, llama2-sft, llama2-dpo, llama2-ppo, pythia-2.8b, pythia-2.8b-sft, pythia-2.8b-dpo]', required=True)
+parser.add_argument('--model', type=str, help='Model must be in [llama2, llama2-chat, llama2-sft, llama2-dpo, llama2-ppo, "llama2-kto", "llama2-slic", pythia-2.8b, pythia-2.8b-sft, pythia-2.8b-dpo]', required=True)
 args = parser.parse_args()
-print(args.accumulate(args.model))
+print(args.model)
+
+max_generation_length_creative = 70
+max_generation_length_factual = 25
 
 model_name = args.model
 
-potential_models = ["llama2", "llama2-chat", "llama2-sft", "llama2-dpo", "llama2-ppo", "pythia-2.8b", "pythia-2.8b-sft", "pythia-2.8b-dpo"]
+potential_models = ["llama2", "llama2-chat", "llama2-sft", "llama2-dpo", "llama2-ppo", "llama2-kto", "llama2-slic", "pythia-2.8b", "pythia-2.8b-sft", "pythia-2.8b-dpo"]
 if model_name not in potential_models:
         raise ValueError(f"Model not set up!")
 
@@ -44,40 +47,76 @@ creative_guides = [" Of course, I'd be happy to help you with a poem! Here is a 
                   " Of course, here is a funny song about llamas:\n\nLlamas",
                   " Okay, I will try to invent a new word.\n\nThe word"]
 
-factual_prompts = ["What is the capital of France?", "How is H2O commonly known?", "What is the largest country in the world?", "How many days are in a year?",
-                   "What is the largest planet in the solar system?", "What is the largest animal in the world?", "How do you say hello in Spanish?", "Who won the 2018 World Cup?",
-                   "What is the biggest city in Europe?", "What is the largest country in Africa?", "What was the last battle of Napoleon?", "How do you call someone from New Zealand?",
-                   "How do you call someone who studies plants?", "Who invented the telephone?", "What mammal lays eggs?", "Which bone is the longest in the human body?", "What is the anthem of France?",
-                   "Who wrote Cannery Row?", "Who was the first president of the United States?", "Which painter painted the Mona Lisa?"]
+factual_prompts = ["What is the capital of France?", 
+                   "How is H2O commonly known?", 
+                   "What is the largest country in the world?", 
+                   "How many days are in a year?",
+                   "What is the largest planet in the solar system?", 
+                   "What is the largest animal in the world?", 
+                   "How do you say hello in Spanish?", 
+                   "Who won the 2018 World Cup?",
+                   "What is the biggest city in Europe?", 
+                   "What is the largest country in Africa?", 
+                   "What was the last battle of Napoleon?", 
+                   "How do you call someone from New Zealand?",
+                   "How do you call someone who studies plants?", 
+                   "Who invented the telephone?", 
+                   "What mammal lays eggs?", 
+                   "Which bone is the longest in the human body?", 
+                   "What is the anthem of France?",
+                   "Who wrote Cannery Row?", 
+                   "Who was the first president of the United States?", 
+                   "Which painter painted the Mona Lisa?"]
 
-factual_guides = [" ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
-                  " ",
+factual_guides = [" Sure let me answer your question. The",
+                  " Sure let me answer your question. It is commonly",
+                  " Sure let me answer your question. The",
+                  " Sure let me answer your question. There",
+                  " Sure let me answer your question. The largest",
+                  " Sure let me answer your question. The",
+                  " Sure let me answer your question. To say",
+                  " Sure let me answer your question. The winner was",
+                  " Sure let me answer your question. The",
+                  " Sure let me answer your question. The largest",
+                  " Sure let me answer your question. The",
+                  " Sure let me answer your question. A nickname for",
+                  " Sure let me answer your question. A",
+                  " Sure let me answer your question. The",
+                  " Sure let me answer your question. The term for",
+                  " Sure let me answer your question. The",
+                  " Sure let me answer your question. The anthem",
+                  " Sure let me answer your question. The writer",
+                  " Sure let me answer your question. The first",
+                  " Sure let me answer your question. It was",
                   ]
+
+factual_ans = [["Paris"], # not case sensitive! Watch for spellings, spaces... 
+              ["water"],
+              ["Russia"],
+              ["365"],
+              ["Jupiter"],
+              ["whale"], # blue whale specifically, models often says elephant
+              ["hola"], # model often adds alternative "buenos dias" meaning "good day"
+              ["France"], # often doesn't answer or says croatia
+              ["Istanbul"], # Usually says London or Moscow. Sometimes says Paris, Rome, etc... 
+              ["Algeria"], # often says Congo or Sudan etc. Only llama chat gets it right suggesting it's in FTing dataset... 
+              ["Waterloo"], # Battle of Waterloo
+              ["Kiwi"], # most models don't get this... 
+              ["botanist"], # pythia not great
+              ["Alexander", "Graham Bell"], # pythia has trouble
+              ["monotreme"], # # pythia and non llama-chat have trouble
+              ["Femur"],
+              ["Marseillaise"], # La Marseillaise
+              ["John Steinbeck", "Steinbeck"], # Pythia doesn't know
+              ["George Washington"],
+              ["Leonardo", "Vinci"]] 
 
 # factual prompts whose answers are longer
 # not used right now
 factual_prompts_longer = ["What separates prehistory from history?", "How were hyerogliphics deciphered?"]
 
-print(len(creative_prompts))
-print(len(factual_prompts))
+# print(len(creative_prompts))
+# print(len(factual_prompts))
 
 # https://github.com/facebookresearch/llama
 # https://github.com/facebookresearch/llama/blob/main/llama/generation.py 
@@ -133,9 +172,8 @@ temperatures = [k / 10. for k in range(1, 16)]
 
 # each temperature and for each prompt, generate n_generations samples
 temperatures = [k / 10. for k in range(1, 16)]
-# pick from "llama2-chat", "llama2", "pythia-2.8b", "pythia-2.8b-sft", "pythia-2.8b-dpo", "pythia-6.9b", "pythia-6.9b-sft", "pythia-6.9b-dpo", "pythia-6.9b-ppo"
-# "llama2-sft", "llama2-dpo", "llama2-ppo"
-# models = ["llama2"] # "llama2", "llama2-chat", "llama2-sft", "llama2-dpo", "llama2-ppo", "pythia-2.8b", "pythia-2.8b-sft", "pythia-2.8b-dpo"
+# models = ["llama2"] 
+# "llama2", "llama2-chat", "llama2-sft", "llama2-dpo", "llama2-ppo", "llama2-kto", "llama2-slic", "pythia-2.8b", "pythia-2.8b-sft", "pythia-2.8b-dpo"
 # print(models)
 n_generations = 25
 completions_creative = np.zeros((len(temperatures), len(creative_prompts), 1), dtype=object)
@@ -143,7 +181,7 @@ completions_factual = np.zeros((len(temperatures), len(factual_prompts), 1), dty
 
 # define the function to be submitted
 # def generate_samples(args):
-def generate_samples(prompt, guide, temperatures, model_name):
+def generate_samples(prompt, guide, temperatures, model_name, max_generation_length):
     max_return_sequences = 5 #for memory reasons, we generate the samples in batches of 5
     # i, prompt, temperatures, model_name = args
     if model_name == "llama2-chat":
@@ -165,6 +203,14 @@ def generate_samples(prompt, guide, temperatures, model_name):
     if model_name == "llama2-ppo":
         tokenizer = AutoTokenizer.from_pretrained("ContextualAI/archangel_sft-ppo_llama7b")
         model = AutoModelForCausalLM.from_pretrained("ContextualAI/archangel_sft-ppo_llama7b") # , torch_dtype=torch.bfloat16 )
+        full_prompt = format_prompt_TuluV2(prompt, guide)
+    if model_name == "llama2-kto":
+        tokenizer = AutoTokenizer.from_pretrained("ContextualAI/archangel_sft-kto_llama7b")
+        model = AutoModelForCausalLM.from_pretrained("ContextualAI/archangel_sft-kto_llama7b") # , torch_dtype=torch.bfloat16 )
+        full_prompt = format_prompt_TuluV2(prompt, guide)
+    if model_name == "llama2-slic":
+        tokenizer = AutoTokenizer.from_pretrained("ContextualAI/archangel_sft-slic_llama7b")
+        model = AutoModelForCausalLM.from_pretrained("ContextualAI/archangel_sft-slic_llama7b") # , torch_dtype=torch.bfloat16 )
         full_prompt = format_prompt_TuluV2(prompt, guide)
     if model_name == "pythia-2.8b":
         tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-2.8b")
@@ -201,7 +247,7 @@ def generate_samples(prompt, guide, temperatures, model_name):
     for temperature in temperatures:
         temp_completions = []
         for _ in range(n_generations // max_return_sequences):
-            samples = model.generate(input_ids, temperature=temperature, max_length=input_ids.shape[1] + 70,
+            samples = model.generate(input_ids, temperature=temperature, max_length=input_ids.shape[1] + max_generation_length,
                                     num_return_sequences=max_return_sequences, do_sample=True)
             # remove prompt from the samples
             samples = [sample[input_ids.shape[1]:] for sample in samples]
@@ -214,23 +260,22 @@ def generate_samples(prompt, guide, temperatures, model_name):
 # os.makedirs("logs", exist_ok=True)
 
 
-for model in models:
+model_completions_creative = []
+for i, prompt in enumerate(creative_prompts): 
+    guide = creative_guides[i]
+    print(f"creative prompt {i}")
+    model_completions = generate_samples(prompt, guide, temperatures, model_name, max_generation_length=max_generation_length_creative)
+    for t_index, completion in enumerate(model_completions):
+        completions_creative[t_index, i, 0] = completion
 
-    model_completions_creative = []
-    for i, prompt in enumerate(creative_prompts): 
-        guide = creative_guides[i]
-        print(f"creative prompt {i}")
-        model_completions = generate_samples(prompt, guide, temperatures, model)
-        for t_index, completion in enumerate(model_completions):
-            completions_creative[t_index, i, models.index(model)] = completion
-
-    # model_completions_factual = []
-    # for i, prompt in enumerate(factual_prompts): 
-    #     print(f"factual prompt {i}")
-    #     model_completions = generate_samples(prompt, temperatures, model)
-    #     for t_index, completion in enumerate(model_completions):
-    #         completions_factual[t_index, i, models.index(model)] = completion
+model_completions_factual = []
+for i, prompt in enumerate(factual_prompts): 
+    guide = factual_guides[i]
+    print(f"factual prompt {i}")
+    model_completions = generate_samples(prompt, guide, temperatures, model_name, max_generation_length=max_generation_length_factual)
+    for t_index, completion in enumerate(model_completions):
+        completions_factual[t_index, i, 0] = completion
 
 # Save the results
-np.save(f'results/{model}_completions_creative_max_length70.npy', completions_creative)
-np.save(f'results/{model}_completions_factual_max_length70.npy', completions_factual)
+np.save(f'results/{model_name}_completions_creative_max_length{max_generation_length_creative}.npy', completions_creative)
+np.save(f'results/{model_name}_completions_factual_max_length{max_generation_length_factual}.npy', completions_factual)
